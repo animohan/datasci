266c7484ea704288056adb04f84415bf4373a701
anishmohan@gmail.com <anishmohan@gmail.com>
2015-08-20 06:49:16 -0700

    added most interest section

--- DataSci/Ch01_01BasisEg.py
+++ DataSci/Ch01_01BasisEg.py
@@ -164,3 +164,69 @@ def friends_of_friend_ids2(user):
 print("\nNew and improved friend of friends")
 for user in users:
 	print (user["name"], friends_of_friend_ids2(user))
+
+#Data showing interest of each id
+interests =[ (0,"Hadoop"),(0,"Big Data"),(0,"Hbase"),(0,"Java"),(0,"Spark"),
+			(0,"Storm"),(0,"Cassandra"),(1,"NoSQL"),(1,"MongoDB"),(1,"Cassandra"),(1,"HBase"),
+			(1,"PostGres"),(2,"Python"),(2,"scikit-learn"),(2,"scipy"),
+			(2,"numpy"),(2,"statmodels"),(2,"pandas"),(3,"R"),(3,"python"),
+			(4,	"machine	learning"),	(4,	"regression"),	(4,	"decision	trees"),
+			(4,	"libsvm"),	(5,	"Python"),	(5,	"R"),	(5,	"Java"),	(5,	"C++"),
+			(5,	"Haskell"),	(5,	"programming	languages"),	(6,	"statistics"),
+			(6,	"probability"),	(6,	"mathematics"),	(6,	"theory"),
+			(7,	"machine	learning"),	(7,	"scikit-learn"),	(7,	"Mahout"),
+			(7,	"neural	networks"),	(8,	"neural	networks"),	(8,	"deep learning"),
+			(8,	"Big Data"),	(8,	"artificial	intelligence"),	(9,	"Hadoop"),
+			(9,	"Java"),(9,"MapReduce"),(9,"Big Data")]
+
+
+#find users with certain interest
+def data_scientists_who_like(target_interest):
+	return [user_id
+			for user_id, user_interest in interests
+			if user_interest==target_interest]
+
+#the above function works, but it searches for entire list fo every search
+# can we build an index from interest to users
+
+from collections import defaultdict
+
+#keys are interest and values are lists for user_ids with that interest
+user_ids_by_interest = defaultdict(list)
+for user_id, interest in interests:
+		user_ids_by_interest[interest].append(user_id)
+
+print(user_ids_by_interest)
+
+#this can be done by mapping from users to interests
+
+#keys are user_ids, values are lists of interests for that user_id
+
+interests_by_user_id = defaultdict(list)
+for user_id, interest in interests:
+		interests_by_user_id[user_id].append(interest)
+
+#now find the interestmatch
+def most_common_interests_with(user):
+	return Counter(interested_user_id
+			for interest in interests_by_user_id[user["id"]]
+			for interested_user_id in user_ids_by_interest[interest]
+			if  interested_user_id!=user["id"])
+
+#lengtheir version of function above
+def most_common_interests_with2(user):
+	common_interest_list=[]
+	for interest in interests_by_user_id[user["id"]]:
+		for interested_user_id in user_ids_by_interest[interest]:
+			if(interested_user_id!=user["id"]):
+				common_interest_list.append(interested_user_id)
+
+	return common_interest_list
+
+
+for user in users:
+	print("\n")
+	print(user["id"],user["name"])
+	print("Common Interests with:")
+	print(most_common_interests_with2(user))
+	print(most_common_interests_with(user))
